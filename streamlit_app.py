738dcefd28e0b8d44501373e00588e8e78b97c2b
unknown <psajeendran@gmail.com>
2024-10-14 17:58:14 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,125 +1,147 @@
-#
-# import numpy as np
-# import pandas as pd
-# import streamlit as st
-# import wfdb
-#
-# # from loguru import logger
-#
-# from page.PreProccessing import Preprocess
-# from page.Descriptive_Analysis import DescriptiveAnalysis
-# from page.Predictive_Analysis import PredictiveAnalysis
-# from utils.sidebar import sidebar_caption
-#
-# #
-# # # Config the whole app
-# # st.set_page_config(
-# #     page_title="A Dashboard Template",
-# #     page_icon="🧊",
-# #     layout="wide",
-# #     initial_sidebar_state="expanded",
-# # )
-# #
-#
-# # @st.cache()
-# def fake_data():
-#     """some fakest.chch data"""
-#
-#     dt = pd.date_range("2021-01-01", "2021-03-01")
-#     df = pd.DataFrame(
-#         {"datetime": dt, "values": np.random.randint(0, 10, size=len(dt))}
-#     )
-#
-#     return df
-#
-#
-# def main():
-#     """A streamlit app template"""
-#
-#     # st.sidebar.title("Tools")
-#     st.set_page_config(layout="wide")
-#     PAGES = {
-#         "Descriptive Analysis": DescriptiveAnalysis,
-#         "PreProcessing": Preprocess,
-#         "Predictive Analysis": PredictiveAnalysis
-#     }
-#
-#     # Select page
-#     # Use dropdown if you prefer
-#     selection = st.sidebar.radio("Tools", list(PAGES.keys()))
-#     sidebar_caption()
-#     option = st.sidebar.selectbox(
-#         "Select A patient Record",
-#         ("100", "101", "102", "103", "104", "105", "106"))
-#
-#     data = f"mit-bih-arrhythmia-database-1.0.0/{option}"
-#
-#     record = wfdb.rdrecord(data, smooth_frames=True)
-#
-#     page = PAGES[selection]
-#
-#     DATA = {"record": record, }
-#
-#     with st.spinner(f"Loading Page {selection} ..."):
-#
-#
-#
-#         page = page(DATA)
-#         page()
-#
-#
-# if __name__ == "__main__":
-#     main()
-
 import streamlit as st
 from page.PreProccessing import Preprocess
+import torch
+import os
 import wfdb
-
+import pandas as pd
+from electroCardioGuard.PersonIdentification import PersonIdentification
 from page.Descriptive_Analysis import DescriptiveAnalysis
-from page.Predictive_Analysis import PredictiveAnalysis
+
 from page.arrythmia_detection import ArrhythmiaAnalysis
+# from page.arrythmia_detection import ArrhythmiaAnalysis
+from page.Predictive_Analysis import PredictiveAnalysis
+from page.Myocardial_Infarction import CNN_LSTM, load_model, predict, encoder,MyocardialInfarction
+from streamlit_extras.metric_cards import style_metric_cards
+
 st.set_page_config(layout="wide")
-# tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+tab1, tab2, tab3 ,tab4,tab5= st.tabs(["📈 Preprocessing", "🗃 Descriptive Analysis", "📊Arrhythmia Detection","📊Mycardioc Infarction Detection", "📊Person Identification"])
+record = None
+# Initialize saved_signals in session state if not already present
+if 'saved_signals' not in st.session_state:
+    st.session_state.saved_signals = []
 
-tab1, tab2, tab3,tab4 = st.tabs(["📈 Preprocessing", "🗃 Descriptive Analysis", "📊 Predictive Analysis","Arrhythia Detection"])
+def save_signal(option, signal):
+    update = False
+    for s in st.session_state.saved_signals:
+        if s["patient_id"] == option :
+            update = True
+            s["signal"] = signal
+            break
+    if not update:
+        t = {"patient_id": option, "signal": signal}
+        st.session_state.saved_signals.append(t)
+    return st.session_state.saved_signals
 
-data = f"mit-bih-arrhythmia-database-1.0.0/100"
-
-record = wfdb.rdrecord(data, smooth_frames=True)
-signal=[]
-# page = PAGES[selection]
-
-DATA = {"record": record, }
 with tab1:
-    option = st.selectbox(
-        "Select A patient Record",
-        ("100", "101", "102", "103", "104", "105", "106"))
+    # select_col1, select_col2 = st.columns([0.5, 0.5])
+    #
+    # with select_col1:
+    #     option = st.selectbox("Select A patient Record", ("100", "101", "102", "103", "104", "105", "106"))
+    #
+    # with select_col2:
+    #     channel = st.selectbox("Select A patient Channel", (0,1))
+    #
 
-    data = f"mit-bih-arrhythmia-database-1.0.0/{option}"
+    # Instructions
+    st.write("""
+        Upload your ECG data files (WFDB format) for analysis.
+    """)
 
-    record = wfdb.rdrecord(data, smooth_frames=True)
-    DATA = {"record": record }
+    # File uploader
+    uploaded_files = st.file_uploader("Choose ECG data files", type=["dat", "hea" ,"xyz"], accept_multiple_files=True)
 
-    page=Preprocess(DATA)
-    signal=page.content()
-    print(signal)
-    # st.header("A cat")
-    # st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    if uploaded_files:
+        for uploaded_file in uploaded_files:
+            if uploaded_file.type == "application/octet-stream":  # Check if it's a 'dat' file
+                dat_file = uploaded_file
+                hea_file = next((f for f in uploaded_files if f.name == uploaded_file.name.replace('.dat', '.hea')),
+                                None)
+                if hea_file:
+                    st.write(f"Uploaded files: {hea_file.name}, {dat_file.name}")
+                    if hea_file.name == dat_file.name:
+                        break
+                    print(f"Uploaded files: {hea_file.name}, {dat_file.name}")
+
+                    # Save uploaded files temporarily
+                    with open(dat_file.name, 'wb') as f:
+                        f.write(dat_file.read())
+                    with open(hea_file.name, 'wb') as f:
+                        f.write(hea_file.read())
+
+                    # Read the record using wfdb
+                    record = wfdb.rdrecord(dat_file.name.replace('.dat', ''))
+
+                    # # Display basic information
+                    # st.write("Record Information:")
+                    # st.write(f"Sampling Frequency: {record.fs} Hz")
+                    # st.write(f"Number of Signals: {record.n_sig}")
+                    # st.write(f"Signal Length: {record.sig_len} samples")
+                    #
+                    # # Plot the first signal
+                    # st.write("ECG Signal:")
+                    # plt.figure(figsize=(10, 4))
+                    # plt.plot(record.p_signal[:, 0])
+                    # plt.xlabel('Sample')
+                    # plt.ylabel('Amplitude')
+                    # plt.title('ECG Signal')
+                    # st.pyplot(plt)
+                    #
+                    # # Optionally, process and display additional information
+                    # # For example, display the first 1000 points of the first signal
+                    # st.write("First 1000 samples of the ECG signal:")
+                    # st.line_chart(record.p_signal[:1000, 0])
+
+                    # Clean up temporary files
+                    os.remove(dat_file.name)
+                    os.remove(hea_file.name)
+
+    #
+                    # data = f"mit-bih-arrhythmia-database-1.0.0/{option}"
+                    # record = wfdb.rdrecord(data, smooth_frames=True)
+                    DATA = {"record": record, "channel": 0, "saved_signals": st.session_state.saved_signals}
+
+                    page = Preprocess(DATA)
+                    signal = page.content()
+
+                    if st.button('Save'):
+                        st.session_state.saved_signals = save_signal(dat_file.name.split('.')[0], signal)
+
+    rows = [[s["patient_id"]] for s in st.session_state.saved_signals]
+    df1 = pd.DataFrame(rows, columns=["patient_id"])
+    st.table(df1)
 
 with tab2:
-    DATA = {"record": record, "signal":signal}
+    if record is None or st.session_state.saved_signals == []:
+        st.error("Please upload a record first.")
+    else:
 
-    page = DescriptiveAnalysis(DATA)
-    page.content()
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+        DATA = {"record": record, "signal": signal,'saved_signals': st.session_state.saved_signals}
+        st.title("ECG Signal Analysis")
+        page = DescriptiveAnalysis(DATA)
+        page.content()
+
 
 with tab3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    if record is None:
+        st.error("Please upload a record first.")
+    else:
+        DATA = {"record": record, "signal": signal, 'saved_signals': st.session_state.saved_signals}
+
+        page = ArrhythmiaAnalysis(DATA)
+        page.content()
 
 with tab4:
-    DATA = {"record": record, "signal":signal}
+    if record is None:
+        st.error("Please upload a record first.")
+    else:
+        DATA = {"record": record, "signal": signal,'saved_signals': st.session_state.saved_signals}
+        page = MyocardialInfarction(DATA)
+        page.content()
 
-    page = ArrhythmiaAnalysis(DATA)
-    page.content()
+with tab5:
+    if record is None:
+        st.error("Please upload a record first.")
+    else:
+        DATA = {"record": record, "signal": signal,'saved_signals': st.session_state.saved_signals}
+        page = PersonIdentification(DATA)
+        page.content()
